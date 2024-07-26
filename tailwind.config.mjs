ec9ac7297ca14dc3b219d4c8e30093ac3cedc5c8
Andrew S Erwin <andrewerwin73@gmail.com>
2024-07-26 04:19:54 -0500

    add global styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+
+export default defineConfig({
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: "class",
 	theme: {
 		extend: {},
 	},
 	plugins: [],
-}
+  integrations: [
+    tailwind({
+      applyBaseStyles: false
+    })
+  ]
+})
